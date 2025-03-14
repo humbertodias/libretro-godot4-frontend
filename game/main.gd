6a70ee81f9e70da76a76f6050507518895f8f240
Humberto <hldtux@gmail.com>
2025-03-13 23:30:58 -0300

    dynamic get_user_data_path

--- game/main.gd
+++ game/main.gd
@@ -31,12 +31,21 @@ func _on_file_dialog_core_file_selected(path):
 	$gd_retro.core_load(path)
 	get_status()
 
+func get_user_data_path():
+	var home_dir = OS.get_environment("HOME")
+	var project_name = ProjectSettings.get_setting("application/config/name")
+	
+	var user_data_path = ""
+	if OS.get_name() == "MacOS":
+		user_data_path = home_dir + "/Library/Application Support/Godot/app_userdata/" + project_name + "/"
+	elif OS.get_name() == "Linux":
+		user_data_path = home_dir + "/.local/share/godot/app_userdata/" + project_name + "/"
+	elif OS.get_name() == "Windows":
+		user_data_path = OS.get_environment("APPDATA") + "/Godot/app_userdata/" + project_name + "/"
+	return user_data_path
+
 func _on_file_dialog_game_file_selected(path: String):
-	# MacOS
-	# ~/Library/Application Support/Godot/app_userdata/GDExtensionLibRetro/
-	# Linux
-	# ~/.local/share/godot/app_userdata/libretro-godot4-frontend/
-	var realpath = path.replace("user://", "/home/tux/.local/share/godot/app_userdata/libretro-godot4-frontend/")
+	var realpath = path.replace("user://", get_user_data_path())
 	print("realpath: %s" % realpath)
 	if $gd_retro.core_load_game(realpath):
 		$ui_canvas_layer/ui/buttons_container_left/button_run.disabled = false
